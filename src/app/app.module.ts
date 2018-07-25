b0554ba9846dd5e929a0ebb1efa59124324980d0
lenkei.akos <lenkei.akos@virgo.hu>
2018-07-25 23:47:50 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponentComponent } from './components/test-component/test-component.component';
+
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+        apiKey: 'AIzaSyCpSFeQHtF4O6lTWP4HF9-o-ZlPcLNuQL0'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
