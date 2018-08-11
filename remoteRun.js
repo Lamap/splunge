094b3d586d6f4246ac3ddc28e6d0e914917371d3
lenkei.akos <lenkei.akos@virgo.hu>
2018-08-11 17:24:29 +0200

    deploy approach to heroku 7

--- remoteRun.js
+++ remoteRun.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+
+app.listen(port);
+
+console.log('remote apps static is: ' + __dirname + '/dist, at port: ' + port);
