71755de41e59038903fd0f72045752d86f391ad9
lenkei.akos <lenkei.akos@virgo.hu>
2018-08-13 17:57:33 +0200

    UI progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -63,7 +63,55 @@ export class AppComponent {
             east: 19.048340,
             west: 19.034961
         }
-    }
+    },
+      {
+          id: 'TK-2000',
+          name: 'Tabán és környéke ezredfordulón',
+          src: 'http://tabanatlas.hspartacus.hu/trkp/K3_25clr.gif',
+          minZoom: 15,
+          maxZoom: 20,
+          isDisplayed: false,
+          opacity: 100,
+          dated: 2000,
+          bounds: {
+              north: 47.495909,
+              south: 47.488665,
+              east: 19.048440,
+              west: 19.034761
+          }
+      },
+      {
+          id: 'TK-1930',
+          name: 'Tabán és környéke a lebontás előtt',
+          src: 'http://tabanatlas.hspartacus.hu/trkp/K2_25clr.gif',
+          minZoom: 15,
+          maxZoom: 20,
+          isDisplayed: false,
+          opacity: 100,
+          dated: 2000,
+          bounds: {
+              north: 47.495909,
+              south: 47.488665,
+              east: 19.048440,
+              west: 19.034761
+          }
+      },
+      {
+          id: 'TK-1900',
+          name: 'Tabán és környéke a századforduló előtt',
+          src: 'http://tabanatlas.hspartacus.hu/trkp/K1_28clr.gif',
+          minZoom: 15,
+          maxZoom: 20,
+          isDisplayed: false,
+          opacity: 100,
+          dated: 2000,
+          bounds: {
+              north: 47.495909,
+              south: 47.488665,
+              east: 19.048440,
+              west: 19.034761
+          }
+      }
   ];
 
   constructor () {}
